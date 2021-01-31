742fa58e2888fe1522d23e82ebac5cfb2558a59b
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-01-31 23:52:39 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,7 +6,7 @@ class LoginForm extends React.Component {
 
     this.state = {};
   }
-  handleSubmit(e) {
+  handleSubmit= e => {
     e.preventDefault()
     if(this.state.username && this.state.password ) return this.props.handleLogin(this.state) }
 
