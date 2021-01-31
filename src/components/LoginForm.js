12ce18988e63ae072bc492396d72c04d23d10e1e
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-01-31 23:52:52 +0000

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,20 +6,35 @@ class LoginForm extends React.Component {
 
     this.state = {};
   }
+  handleSubmit= e => {
+    e.preventDefault()
+    if(this.state.username && this.state.password ) return this.props.handleLogin(this.state) }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              value={this.state.username}
+              onChange={(e) => this.setState({ username: e.target.value })}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -8,10 +8,13 @@ class TwitterMessage extends React.Component {
   }
 
   render() {
+    console.log(this.props.maxChars)
+    console.log(this.state)
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input value={this.state.message} onChange={(e) => this.setState({message: e.target.value})} type="text" name="message" id="message" />
+        <p>{this.state.message ? this.props.maxChars-this.state.message.length: 280}</p>
       </div>
     );
   }
